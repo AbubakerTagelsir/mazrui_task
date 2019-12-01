5dc095ff469bc91a47ebc94db93d48ab754213cb
Abubaker Tagelsir <abubaker.tagelsir@gmail.com>
2019-12-01 17:22:00 +0400

    remove some default files initial setup react, redux

---
+++ react-mini-social-network/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <div>
+            Hi There!
+        </div>
+    );
+};
+
+export default App;
