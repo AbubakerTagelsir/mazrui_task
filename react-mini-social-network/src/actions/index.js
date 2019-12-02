7e81940565bea09be532949985d53a0ad4e8268e
Abubaker Tagelsir <abubaker.tagelsir@gmail.com>
2019-12-02 06:30:31 +0400

    redirect react requests using proxy setup redux thunk

---
+++ react-mini-social-network/src/actions/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import {FETCH_USER} from './types';
+import { type } from 'os';
+
+const fetchUser = () => {
+    return function (dispatch){
+        axios
+            .get('/api/current_user')
+            .then(res => dispatch({type: FETCH_USER, payload: res}));
+    }
+};
